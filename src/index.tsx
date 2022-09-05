b133df3827645b92aa3c15432292e4fc6c2ace13
toluamole <amoletoluwanimi@gmail.com>
2022-09-05 11:06:58 +0100

    fix react dom error

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootElement = document.getElementById('root');
+// eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+const root = createRoot(rootElement!);
 
-ReactDOM.render(
-	<React.StrictMode>
+
+root.render(
+	<StrictMode>
 		<ColorModeScript />
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root')
+	</StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
